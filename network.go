b598c777e3125b689222fee910221a240a2cda48
maswalker <handsome.void@gmail.com>
2024-12-04 04:04:34 +0000

    add

--- network.go
+++ network.go
@@ -1,3 +1,18 @@
 package goweb3
 
-type ()
+import (
+	"context"
+
+	"github.com/meme-bots/go-web3/evm"
+	"github.com/meme-bots/go-web3/sol"
+	"github.com/meme-bots/go-web3/types"
+)
+
+func NewNetwork(ctx context.Context, cfg types.Config) (types.NetworkInterface, error) {
+	if cfg.Type == types.NetworkTypeSol {
+		return sol.NewSolana(ctx, &cfg)
+	} else if cfg.Type == types.NetworkTypeEVM {
+		return evm.NewEVM(ctx, &cfg)
+	}
+	return nil, types.ErrNotImplemented
+}
